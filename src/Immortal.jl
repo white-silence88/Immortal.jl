ca5a340b8abac8c1dce69da2f2271bb7f6b40b9c
Dmitrii Shevelev <igrave1988@gmail.com>
2020-08-15 23:45:44 +0500

    Удалил рудименты кода

--- src/Immortal.jl
+++ src/Immortal.jl
@@ -9,8 +9,6 @@ module Immortal
     function run(login::String, password::String, vhost::String, port::Int64)
         host::String = "api.seon.cloud"
         is_connected::Bool = false
-
-        i::Int16 = 0
         connection = nothing
 
         try
@@ -18,11 +16,10 @@ module Immortal
             connection = Rabbit.get_rabbitmq_connection(vhost, host, port, auth_params)
             chanel = Rabbit.get_chanel(connection, nothing, true)
             is_connected = true
-            println(chanel)
-            Rabbit.shotdown(connection)
         catch e
             print(e)
-            exit()
         end
+
+        Rabbit.shotdown(connection)
     end
 end
