9d89e0c6abb7c866da130dcb43b92dc270aeebbe
Dmitrii Shevelev <igrave1988@gmail.com>
2020-09-10 17:05:38 +0500

    Добавил функцию чтения из JSON файла

--- src/utils/src/FileReader.jl
+++ src/utils/src/FileReader.jl
@@ -1,8 +1,36 @@
 module FileReader
     using YAML
+    using JSON
+
     include("./Chronometer.jl")
     import .Chronometer
-    
+
+    """Функция импорта настроек из JSON файла
+
+    file_path: путь до файла.
+    """
+    function from_json(file_path::String)
+        @info Chronometer.message_with_time("Выполняется функция чтения данных из JSON файла...")
+        @debug "Параметры вызова функции:"
+        @debug "- путь до файла в файловой системе (полный путь)" file_path
+        result = nothing
+        @debug "Инициализируем результат с пустым значенем" result
+        try
+            @debug "Пытаемся открыть файл в файловой системе"
+            file = open(file_path)
+            @debug "Результат открытия файла" file
+            @debug "Пытаемся прочитать данные из файла"
+            data = read(file, String)    
+            @debug "Результат чтения данных из файла" data
+            @debug "Пытаемся распарсить строку JSON в словарь"
+            result = JSON.parse(data)
+            @debug "Результат парсинга JSON строки" result
+        catch error
+            @error "Не удалось прочитать JSON файл. Ошибка: " error
+        end
+        return result
+    end
+
     """Функция импорта настроек из YAML файла
 
     file_path: путь до файла.
